22f3508d455c105711fcedd1c3301cb936ce6891
shaoyingzhu <yingzhu.shao@newtouch.cn>
2018-05-03 14:41:20 +0800

    初始化项目

---
+++ src/main/java/com/example/springCloudStreamRabbitMQ/SpringCloudStreamRabbitMqApplication.java
@@ -0,0 +1,12 @@
+package com.example.springCloudStreamRabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamRabbitMqApplication.class, args);
+	}
+}
